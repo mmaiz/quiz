bdb84bff1eb3de1b424cbc5cdc15da15036e6cba
mmaiz <mikel.maiz.ruiz@gmail.com>
2015-07-17 11:56:13 +0200

    Validación de preguntas

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors: []});
 });
 
 /* GET author's page */
 router.get('/author', function(req, res) {
-	res.render('author', { title: 'Créditos' });
+	res.render('author', { title: 'Créditos', errors: []});
 });
 
 // Autoload de comandos con :quizId
