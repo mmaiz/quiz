297a832ba6a77550e5b0478a0e166edc91eceefc
mmaiz <mikel.maiz.ruiz@gmail.com>
2015-07-22 10:46:46 +0200

    Temas ejercicio 8

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -44,7 +44,7 @@ exports.answer = function(req, res) {
 // GET /quizes/new
 exports.new = function(req, res) {
   var quiz = models.Quiz.build( // crea objeto quiz 
-    {pregunta: "Pregunta", respuesta: "Respuesta"}
+    {pregunta: "Pregunta", respuesta: "Respuesta", tema: "Tema"}
   );
 
   res.render('quizes/new', {quiz: quiz, errors: []});
@@ -62,8 +62,8 @@ exports.create = function(req, res) {
         res.render('quizes/new', {quiz: quiz, errors: err.errors});
       } else {
         quiz // save: guarda en DB campos pregunta y respuesta de quiz
-        .save({fields: ["pregunta", "respuesta"]})
-        .then( function(){ res.redirect('/quizes')}) 
+        .save({fields: ["pregunta", "respuesta", "tema"]})
+        .then( function(){ res.redirect('/quizes'); console.log('\nCreate !!! \n');}) 
       }      // res.redirect: Redirección HTTP a lista de preguntas
     }
   ).catch(function(error){next(error)});
@@ -80,6 +80,7 @@ exports.edit = function(req, res) {
 exports.update = function(req, res) {
   req.quiz.pregunta  = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
 
   req.quiz
   .validate()
@@ -89,8 +90,8 @@ exports.update = function(req, res) {
         res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
       } else {
         req.quiz     // save: guarda campos pregunta y respuesta en DB
-        .save( {fields: ["pregunta", "respuesta"]})
-        .then( function(){ res.redirect('/quizes');});
+        .save( {fields: ["pregunta", "respuesta", "tema"]})
+        .then( function(){ res.redirect('/quizes'); console.log('\nUpdate !!! \n');});
       }     // Redirección HTTP a lista de preguntas (URL relativo)
     }
   ).catch(function(error){next(error)});
--- models/models.js
+++ models/models.js
@@ -32,15 +32,15 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
     // success(..) ejecuta el manejador una vez creada la tabla
-    Quiz.count().success(function (count){
+    Quiz.count().then(function (count){
         if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.bulkCreate( 
-        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma'},
-          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa'},
-          {pregunta: 'Capital de España', respuesta: 'Madrid'},
-          {pregunta: 'Capital de Francia', respuesta: 'París'}
+        [ {pregunta: 'Capital de Italia',   respuesta: 'Roma', tema: 'Humanidades'},
+          {pregunta: 'Capital de Portugal', respuesta: 'Lisboa', tema: 'Ocio'},
+          {pregunta: 'Capital de España', respuesta: 'Madrid', tema: 'Ciencia'},
+          {pregunta: 'Capital de Francia', respuesta: 'París', tema: 'Humanidades'}
         ]
       ).then(function(){console.log('Base de datos inicializada')});
         };
